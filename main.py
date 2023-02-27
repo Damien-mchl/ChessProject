2bf98740b9f83a06456002ed44cf911a915e7744
E184393C <axel.raballand@etu.univ-nantes.fr>
2023-02-27 11:12:18 +0100

    venv

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("test")
+import sys    
+from PyQt5 import QtWidgets, QtCore  
+
+app = QtWidgets.QApplication(sys.argv)  
+widget = QtWidgets.QWidget()  
+widget.resize(400, 200)  
+widget.setWindowTitle("This is PyQt Widget example")  
+widget.show()        
+exit(app.exec_())
